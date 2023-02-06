f25e45d544bf7d57ff622d577935b62b5463d258
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-02-05 22:27:33 -0400

    added chat screen.

--- components/Chat.js
+++ components/Chat.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { StyleSheet, View, Text, Platform, KeyboardAvoidingView  } from 'react-native';
 
 export default class Chat extends React.Component {
-    componentDidMount(){
-        let name = this.props.route.params.name; // OR ...
-        this.props.navigation.setOptions({ title: name });
+constructor(props) {
+    super(props);
+    this.state = {
+        messages: [],
     }
-
-    render() {
-
-        return (
-            <View style={styles.container}>
-            {/* Rest of the UI */}
-            </View>
-        );
-    };
+   
 }
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+//shows a system message saying you "user" entered the chat
+//shows a message from "user's friend" saying Hello developer.
+    componentDidMount() {
+        let name = this.props.route.params.name;
+        this.props.navigation.setOptions({ title: name });        this.setState({
+          messages: [
+            {
+                _id: 2,
+                text: `${name} has entered the chat`,
+                createdAt: new Date(),
+                system: true,
+               },
+            {
+              _id: 1,
+              text: 'Hello developer',
+              createdAt: new Date(),
+              user: {
+                _id: 2,
+                name: 'React Native',
+                avatar: 'https://placeimg.com/140/140/any',
+              },
+            },
+            
+          ],
+        })
+      }
+      //sends new message along with old messages
+      onSend(messages = []) {
+        this.setState(previousState => ({
+          messages: GiftedChat.append(previousState.messages, messages),
+        }))
+      }
+      //makes users' message bubble the color black
+      renderBubble(props) {
+        return(
+            <Bubble
+            {...props}
+            wrapperStyle={{
+                right: {
+                    backgroundColor: '#000'
+                }
+            }}
+            />
+        )}
+      render(){
+        return(
+            <View style={{flex: 1}}>
+            <GiftedChat
+            renderBubble={this.renderBubble.bind(this)}
+            messages={this.state.messages}
+            onSend={messages => this.onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+          />
+    {/* if the user is using an android device, 
+    this makes it so their keyboard dowsn't hide the chat field */}
+          { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+          </View>
+        )
+      }
+     
+   }
+
+
+
