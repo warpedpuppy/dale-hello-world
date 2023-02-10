358615c0afc7e6c5fbf9b32ac40d6109b7b909fd
Waledilson <95579186+Waledilson@users.noreply.github.com>
2023-02-10 11:08:10 -0400

    looks like everything works

--- components/Chat.js
+++ components/Chat.js
@@ -14,10 +14,13 @@ export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      loggedInText: "",
       messages: [],
       uid: 0,
-      loggedInText: "",
-      user: {},
+      user: {
+        _id: "",
+        name: "",
+      },
     };
     // if (!firebase.apps.length) {
     //   firebase.initializeApp(firebaseConfig);
@@ -30,32 +33,54 @@ export default class Chat extends React.Component {
     //   messagingSenderId: "12408759444",
     //   appId: "1:12408759444:web:e2121e2f62c2210a6b4081",
     // };
-
-    firebase.initializeApp({
-      apiKey: "AIzaSyAHi1wCLcVwD4N1vLC59MThCugZaytXT9Q",
-      authDomain: "hello-world-8932e.firebaseapp.com",
-      projectId: "hello-world-8932e",
-      storageBucket: "hello-world-8932e.appspot.com",
-      messagingSenderId: "12408759444",
-      appId: "1:12408759444:web:e2121e2f62c2210a6b4081",
-    });
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyAHi1wCLcVwD4N1vLC59MThCugZaytXT9Q",
+        authDomain: "hello-world-8932e.firebaseapp.com",
+        projectId: "hello-world-8932e",
+        storageBucket: "hello-world-8932e.appspot.com",
+        messagingSenderId: "12408759444",
+        appId: "1:12408759444:web:e2121e2f62c2210a6b4081",
+      });
+    }
 
     this.referenceChatMessages = firebase.firestore().collection("messages");
-    // .where("uid", "==", this.state.uid);
   }
 
+  //
   componentDidMount() {
+    //sets user's name is header
     let name = this.props.route.params.name;
+    this.setState({
+      messages: [
+        {
+          _id: 2,
+          loggedInText: `${name} has entered the chat`,
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
     this.props.navigation.setOptions({ title: name });
-    this.referenceChatMessages = firebase.firestore().collection("messages");
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+    // create a reference to the active user's documents
+    this.referenceChatMessagesUser = firebase
+      .firestore()
+      .collection("messages")
+      .where("uid", "==", this.state.uid);
+    this.unsubscribe = this.referenceChatMessages.onSnapshot(
+      this.onCollectionUpdate
+    );
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
       if (!user) {
-        await firebase.auth().signInAnonymously();
+        firebase.auth().signInAnonymously();
       }
+      // update user state with current user data
       this.setState({
-        uid: user.uid,
-        loggedInText: "Hello There",
+        uid: user?.uid,
+        messages: [],
       });
+      // listen for collection changes for current user
       this.unsubscribe = this.referenceChatMessages
         .orderBy("createdAt", "desc")
         .onSnapshot(this.onCollectionUpdate);
@@ -67,12 +92,6 @@ export default class Chat extends React.Component {
     this.authUnsubscribe();
   }
 
-  //sends new message along with old messages
-  onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
-  }
   onCollectionUpdate = (querySnapshot) => {
     const messages = [];
     // go through each document
@@ -83,14 +102,30 @@ export default class Chat extends React.Component {
         _id: data._id,
         text: data.text,
         createdAt: data.createdAt.toDate(),
-        user: data.user,
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+        },
       });
-      this.setState({
-        messages,
-      });
+    });
+    this.setState({
+      messages,
     });
   };
 
+  //sends new message along with old messages
+  onSend(messages = []) {
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessage();
+      }
+    );
+  }
+
+  //adds message to chat
   addMessage = () => {
     const message = this.state.messages[0];
     this.referenceChatMessages.add({
@@ -98,10 +133,11 @@ export default class Chat extends React.Component {
       _id: message._id,
       text: message.text || "",
       createdAt: message.createdAt,
+      user: message.user,
     });
   };
 
-  //makes users' message bubble the color black
+  //makes users' message bubble the color black and the other users' grey
   renderBubble(props) {
     return (
       <Bubble
@@ -110,19 +146,24 @@ export default class Chat extends React.Component {
           right: {
             backgroundColor: "#000",
           },
+          left: {
+            backgroundColor: "#eee",
+          },
         }}
       />
     );
   }
   render() {
+    let backgroundColor = this.props.route.params.color;
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, { backgroundColor }]}>
+        <Text>{this.state.loggedInText}</Text>
         <GiftedChat
           renderBubble={this.renderBubble.bind(this)}
           messages={this.state.messages}
           onSend={(messages) => this.onSend(messages)}
           user={{
-            _id: 1,
+            _id: this.state.uid,
           }}
         />
         {/* if the user is using an android device, 
@@ -149,7 +190,3 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
 });
-
-// run it with expo check the errors
-// somethign with uid
-// keep checkin other codes i guess
